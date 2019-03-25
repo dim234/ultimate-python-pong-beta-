6b58ea500bfe507410bd5e3ab41ec1970b14d073
dim234 <Mitsos06@outlook.com>
2019-03-25 19:41:42 +0200

    Update pong (BETA).py

--- pong (BETA).py
+++ pong (BETA).py
@@ -47,7 +47,17 @@ pen.goto(0, 260)
 pen.write("player a: 0 player b: 0", align="center", font=("Courier", 24, "normal"))
 
 
+def score_a():
+    pen.write("player a: {}", align="center", font=("Courier", 24, "normal"))
+
+
+def score_b():
+    pen.write("player b: {}", align="center", font=("Courier", 24, "normal"))
+
+
 # function
+
+
 def paddle_a_up():
     y = paddle_a.ycor()
     y += 20
@@ -102,14 +112,14 @@ while True:
         ball.dx *= -1
         score_a += 1
         pen.clear()
-        pen.write("player a: 0 {} player b: 0 {}".format(score_a, scoa_b), align="center", font=("Courier", 24, "normal"))
+        pen.write("player a: {} player b: {}".format(score_a, score_b), align="center", font=("Courier", 24, "normal"))
 
     if ball.xcor() < -390:
         ball.goto(0, 0)
         ball.dx *= -1
-        score_b +=1
+        score_b += 1
         pen.clear()
-        pen.write("player a: 0 {} player b: 0 {}".format(score_a, scoa_b), align="center", font=("Courier", 24, "normal"))
+        pen.write("player a: {} player b: {}".format(score_a, score_b), align="center", font=("Courier", 24, "normal"))
 
     # Paddle and ball collisions
     if ball.xcor() > 340 and (paddle_b.ycor() + 40 > ball.ycor() > paddle_b.ycor() - 40):
@@ -119,5 +129,3 @@ while True:
     if ball.xcor() < -340 and (paddle_a.ycor() + 40 > ball.ycor() > paddle_a.ycor() - 40):
         ball.setx(-340)
         ball.dx *= -1
-
-    # Scoring system
